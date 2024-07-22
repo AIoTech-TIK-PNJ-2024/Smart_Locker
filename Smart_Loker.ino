583f7267581147e039e341860aa008043594e898
Gerry Satria Halim <108446193+GerrysatriaH@users.noreply.github.com>
2024-07-22 13:54:46 +0700

    Update Smart_Loker.ino

--- Smart_Loker.ino
+++ Smart_Loker.ino
@@ -1,11 +1,17 @@
 #include <SPI.h>
 #include <MFRC522.h>
+#include <WiFi.h>
+#include <WebServer.h>
 
 #define SS_PIN 21   // SDA
 #define RST_PIN 22  // RST
 #define BUZZER_PIN 26   // Buzzer
 #define LED_PIN 27      // LED
 #define SOLENOID_PIN 5  // Solenoid Door Lock
+
+const char* ssid = "G";
+const char* password = "12345678";
+WebServer server(80);
  
 MFRC522 rfid(SS_PIN, RST_PIN);
 MFRC522::MIFARE_Key key; 
@@ -23,9 +29,22 @@ void setup() {
     key.keyByte[i] = 0xFF;
   }
   digitalWrite(SOLENOID_PIN, HIGH);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  
+  Serial.println("Connected to WiFi");
+  Serial.println(WiFi.localIP());
+
+  server.on("/unlock", handleUnlock);
+  server.begin();
 }
  
 void loop() {
+  server.handleClient();
   if (!rfid.PICC_IsNewCardPresent())
     return;
 
@@ -101,6 +120,18 @@ void loop() {
 
   rfid.PICC_HaltA();
   rfid.PCD_StopCrypto1();
+}
+
+void handleUnlock() {
+  tone(BUZZER_PIN,1000);
+  digitalWrite(LED_PIN, HIGH);
+  delay(200);
+  noTone(BUZZER_PIN);
+  digitalWrite(SOLENOID_PIN,LOW);    
+  delay(3000);
+  digitalWrite(SOLENOID_PIN,HIGH);
+  digitalWrite(LED_PIN, LOW);
+  server.send(200, "text/plain", "Locker unlocked");
 }
 
 void printHex(byte *buffer, byte bufferSize) {
